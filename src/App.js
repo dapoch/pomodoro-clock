caa68bd8d92a1cb5dbc3eeb577fc064a7fd66e05
dapoch <pmscasso@gmail.com>
2021-11-17 11:02:33 -0300

    Added reset button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Break from './components/break';
 import Session from './components/session';
 import TimeLeft from './components/TimeLeft';
+import { useEffect } from "react";
 
 function App() {
-
+  const [currentSession, setCurrentSession] = useState("Session");
+  const [intervalId, setIntervalId] = useState(null);
   const [sessionLength, setSessionLength] = useState(60 * 25)
-
+  const [breakLength, setBreakLength] = useState(300)
+  const [timeLeft, setTimeLeft] = useState(sessionLength);
+  // change timeLeft whenever sessionLength changes
+  useEffect(()=>{
+      setTimeLeft(sessionLength)
+  }, [sessionLength]);
   const decrementSessionLength = () => {
       const newSessionLength = sessionLength - 60;
       if (newSessionLength < 0) {
@@ -21,7 +28,40 @@ function App() {
       setSessionLength(sessionLength + 60)
   }
   
-  const [breakLength, setBreakLength] = useState(300)
+  const isStarted = intervalId !== null;
+    const handleStartStop = () => {
+        if (isStarted) {
+        // if we are in started mode:
+        //we want to stop the timer
+        //we use clearInterval
+        clearInterval(intervalId)
+        setIntervalId(null)
+        } else {
+        // if we are in stop mode we
+        // decrement timeLeft by one every second (1000 ms)
+        // to do this we use setInterval
+        const newIntervalId = setInterval(()=>{
+            setTimeLeft(prevTimeLeft => {
+                if (prevTimeLeft - 1 >= 0){
+                    return prevTimeLeft - 1;
+                }
+                if (currentSession === "Session") {
+                    setCurrentSession("Break");
+                    setTimeLeft(breakLength);
+                } else if (currentSession === "Break") {
+                    setCurrentSession("Session");
+                    setTimeLeft(sessionLength);
+                }
+                
+            });
+        }, 100);
+        setIntervalId(newIntervalId)
+    };
+            }
+
+
+
+  
 
     const decrementBreakLength = () => {
         const newBreakLength = breakLength - 60;
@@ -36,7 +76,21 @@ function App() {
         setBreakLength(breakLength + 60)
     }
 
+    const handleResetButton = () => {
+        // clear timeout interval
+        clearInterval(intervalId)
+        // set intervalId null
+        setIntervalId(null)
+        // set the sessiontype to "session"
+        setCurrentSession("Session")
+        // reset the session length to 25 minutes
+        setSessionLength(60 * 25)
+        // reset the break length
+        setBreakLength(60 * 5)
+        // reset timer to 25 minutes
+        setTimeLeft(60 * 25)
 
+    }
 
   return (
     <div className="App">
@@ -45,13 +99,22 @@ function App() {
       incrementBreakLength={incrementBreakLength}
       decrementBreakLength={decrementBreakLength}
        />
-      <TimeLeft breakLength={breakLength} sessionLength={sessionLength} />
+      <TimeLeft 
+      breakLength={breakLength} 
+      timerLabel={currentSession} 
+      sessionLength={sessionLength}
+      handleStartStop={handleStartStop}
+      startStopLabel={isStarted? "Stop" : "Start"}
+      timeLeft={timeLeft}
+
+       />
       <Session
       sessionLength={sessionLength}
       decrementSessionLength={decrementSessionLength}
       incrementSessionLength={incrementSessionLength}
 
        />
+       <button id="reset" onClick={handleResetButton}>Reset</button>
     </div>
   );
 }
--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,58 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import moment from "moment";
 import momentDurationFormatSetup from "moment-duration-format";
 
 momentDurationFormatSetup(moment)
 
 const TimeLeft = ({
-    sessionLength, breakLength
+    timerLabel, 
+    handleStartStop,
+    startStopLabel,
+    timeLeft
 }) => {
-    const [currentSession, setCurrentSession] = useState("Session");
-    const [ intervalId, setIntervalId] = useState(null);
-    const [ timeLeft, setTimeLeft ] = useState(sessionLength);
-    // change timeLeft whenever sessionLength changes
-    useEffect(()=>{
-        setTimeLeft(sessionLength)
-    }, [sessionLength]);
-    const isStarted = intervalId !== null;
-    const handleStartStop = () => {
-        if (isStarted) {
-        // if we are in started mode:
-        //we want to stop the timer
-        //we use clearInterval
-        clearInterval(intervalId)
-        setIntervalId(null)
-        } else {
-        // if we are in stop mode we
-        // decrement timeLeft by one every second (1000 ms)
-        // to do this we use setInterval
-        const newIntervalId = setInterval(()=>{
-            setTimeLeft(prevTimeLeft => {
-                if (prevTimeLeft - 1 >= 0){
-                    return prevTimeLeft - 1;
-                }
-                if (currentSession === "Session") {
-                    setCurrentSession("Break");
-                    setTimeLeft(breakLength);
-                } else if (currentSession === "Break") {
-                    setCurrentSession("Session");
-                    setTimeLeft(sessionLength);
-                }
-                
-            });
-        }, 100);
-        setIntervalId(newIntervalId)
-    };
-            }
 
+
+    
     
 
     const formattedTimeLeft = moment.duration(timeLeft, "s").format("mm:ss", {trim: false})
     return (
         <div>
-           <p id="timer-label">{currentSession}</p>
+           <p id="timer-label">{timerLabel}</p>
            <p id="time-left">{formattedTimeLeft}</p>
-           <button onClick={handleStartStop}>{isStarted? "Stop" : "Start"}</button>
+           <button onClick={handleStartStop}>{startStopLabel}</button>
         </div>
     )
 }
